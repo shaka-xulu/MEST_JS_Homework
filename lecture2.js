1f479d9ffa58ea3d7eba901d7af73ca8a749cb59
Sakhile Xulu <gxabhashe430@gmail.com>
2016-10-30 23:09:06 +0000

    ready for PR

--- lecture2.js
+++ lecture2.js
@@ -1,10 +1,169 @@
 /*
  * 1. explain the special meaning of the following special characters
- \0 the null character
- \n new line
- \r carriage return
- \v vertical tab
- \t
- \backspace
- \f form feed
+ \0 the null character : Finds the postition of null character
+ \n new line : Breaks a line and continues on the next line
+ \r carriage return - Tells cursor to jump to next line
+ \v vertical tab  - Returns the position where th vertical tab character was found
+ \t tab character - Retunrs the position where the tab character was found
+ \backspace - 
+ \f form feed - Returns position where the form feeder was found. Goes to the next page
  */
+
+
+ //  2.Please combine those two array together as one array by using concate:
+   var arr1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday']
+   var arr2 = ['Thursday', 'Friday', 'Saturday']
+ 	var arr3 = arr1.concat(arr2);
+ 
+
+
+ //3.Please combine those three array together as one array by only using concate() function once:
+  var arr1 = ['Sunday', 'Monday']
+   var arr2 = ['Tuesday', 'Wednesday']
+   var arr3 = ['Thursday', 'Friday', 'Saturday']
+   var arr4 = arr1.concat(arr2,arr3);
+ 
+
+
+ // what is the result(output of following code):
+     var arr = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
+     console.log(arr.sort());
+     console.log(arr);
+ 	/*[ 'Enoch', 'Todd', 'Yaw', 'ashwin', 'jerry', 'lars', 'mike' ] 
+ 	the .sort() function has changed in alphabetical order and Uppercase take precedence over lowercase */
+
+ 	// [ 'Enoch', 'Todd', 'Yaw', 'ashwin', 'jerry', 'lars', 'mike' ] 
+
+
+//  5. what is the result(output of following code):
+     var arr = [1, 3, 'apple', 'orange', 'black', 'test', 2];
+     console.log(arr.sort());
+     console.log(arr);
+     //[ 1, 2, 3, 'apple', 'black', 'orange', 'test' ]
+ 
+
+
+ //6. Please remove mike, jerry, adn Enoch from the following array by using slice
+    var arrx = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
+   	console.log(arrx.slice(2,4));
+    console.log(arrx.slice(5,6));
+
+
+ 	
+
+
+ //7. what is the result(output of following code):
+   var arry = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
+   console.log(arry.slice(-3, -1)); // Outputting Yaw & Enoch. Cause its starting from the end of the array...
+   console.log(arry);
+ 
+
+
+
+ //* 8. what is the result(output of following code):
+   var arrz = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
+   console.log(arrz.slice(2)); // Outputting the results from index 2 till the end.
+   console.log(arrz);
+ 
+
+
+  //9. what is the result(output of following code):
+     var arra = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
+     console.log(arra.slice(-2)); // Outputting the last 2 results of the array.
+    console.log(arra);
+
+
+ // 10. convert the following function to function expression:
+    
+var sums = function addTwoNumber (num1, num2) {
+	 
+      console.log(num1 + num2);
+
+    }
+    sums(265, 234);
+   
+ 
+ // and invoke the function expression to caluate the result of 256 + 532
+ 
+
+/*
+ * 11. create an immediately invoked function expression
+ * store the iife within a variable and call it
+ */
+
+
+// 12. you have following objects: use bind function here to make namer() function to call 'ashwin' and lars
+var person1 = {
+      name: 'ashwin'
+ };
+ 
+ var person2 = {
+      name: 'lars'
+ };
+ 
+function namer() {
+      return this.name;
+}
+
+name1 = namer.bind(person1);
+console.log(name1());
+
+name2 = namer.bind(person2);
+console.log(name2());
+
+
+
+  //12. you have following object:
+      var number = {
+           x: 22,
+           y: 33
+      };
+ 
+     var count = function() {
+           console.log(this.x + this.y);
+    }
+    numb = count.bind(number);
+    console.log(numb());
+ /*  a. what is the result when we invoke count()?
+ *  b. use bind on function expression to make this work.
+ */
+
+
+ //* 13. you have following objects:
+    var personb = {
+           firstName: 'ashwin',
+          lastName: 'Yaw'
+   };
+ 
+    var personz = {
+           firstName: 'lars',
+          lastName: 'Mike'
+     };
+ 
+      function hello(greeting) {
+           console.log(this.greeting + ' ' + this.firstName + ' ' + this.lastName);
+     }
+
+   hello.call(personb);
+   hello.call (personz);
+
+
+ 
+  // use call methods on function expression to make this work.
+ 
+
+/*
+ * 14. We have a following string:
+ *     var str = 'Training, mentoring, and investing in world-class tech entrepreneurs in Africa.'
+ *
+ *     please write a function to change the first letter of every word to upper case.
+ */
+    var str = 'training, mentoring, and investing in world-class tech entrepreneurs in Africa.';
+
+function capitalise(str) {
+      console.log(str.replace(/\b\w/g, x => x.toUpperCase()));
+ }
+ capitalise(str);
+
+
+
--- lecture3.js
+++ lecture3.js
@@ -1,3 +1,124 @@
 /**
  * Created by Jerry on 2016-10-22.
  */
+
+/*  1. Explain the following code:
+ *
+ *  function func() {
+ *      console.log(this);
+ *  }
+ *
+ *  func(); // what is the output? Why?
+ *
+ *  var obj = {
+ *      func: func // what is the second func here?
+ *  };
+ *
+ *  obj.func(); //what is the output? try to explain why.
+ *
+ *  obj.func.bind(window)(); // what is the output? why?
+ *
+ *  obj.func.bind(this)();  // what is the output? why?
+ *
+ *  var people = {
+ *      name: 'ashwin',
+ *      age: 40
+ *  };
+ *
+ *  obj.func.bind(people)(); //what is the output? why?
+ */
+//ANSWERS:
+/*
+//--1a)
+function func() {
+     console.log(this);
+}
+func();
+
+OUTPUT:
+Window
+
+EXPLANATION:
+The function, func, is declared in the global scope (as it is declared outside a function), which is the window level scope.
+So, THIS, points/refers to the window object
+
+//--1b)
+var obj = {
+	func: func
+};
+EXPLANATION:
+The second func is the func declared in 1a but its in the scope of the obj object. 'this' refers to the object in this context
+
+//--1c)
+obj.func();
+EXPLANATION:
+The output is the description of an object's properties and methods. In this case, it is obj.
+The func method is called within the obj context/scope. Hence, this. refers to the obj object
+
+//--1d)
+obj.func.bind(window)();
+OUTPUT:
+window
+
+EXPLANATION:
+It binds the function, func, to the window object.
+So, this, refers to the window object
+
+//--1e)
+obj.func.bind(this)();
+OUTPUT:
+window
+
+EXPLANATION:
+same as above. this is the the global scope which is the window level scope
+
+//--1f)
+var people = {
+	name: 'ashwin',
+	age: 40
+};
+obj.func.bind(people)();
+OUTPUT:
+Object {name: "ashwin", age: 40}
+
+EXPLANATION:
+It binds the func function to the people object.
+So, this, is in the people object scope/context.
+*/
+
+
+
+
+
+/* 2. write a function to convert first letter of every word in a string
+ *     to upper case.
+ *     eg input = 'hi, welcome to mest;'
+ *        output = 'Hi Welcome To Mest';
+ *        input2 = 'today is Sunday';
+ *        output2 = 'Today is Sunday';
+ 	ANSWER:
+ 	input = "hi, welcome to mest";
+input = input.split(" ");
+for(var i = 0; i < input.length; i++){
+	console.log(input[i].slice(0,1).toUpperCase() + input[i].slice(1));
+}
+ */
+
+/*
+ *  3. Write reg expresion for email validation(This is not an easy
+ *  question, please make sure you do your research on this).
+ *  ANSWER:
+
+ */
+
+/*
+ * 4. what is localStorage and sessionStorge under window object? What is the
+ *    difference
+ 	ANSWER:
+ 	For local storage, the data stored in localStorage continues until completely deleted.
+    Changes made are saved and available for all current and future visits to the site.
+
+    For sessionStorage, changes are only available on each window (or tab in browsers like Chrome and Mozila).
+    Changes made are saved and available for the current page, as well as future visits to the site on the same window.
+    Once the window is closed, the storage is deleted.
+ */
